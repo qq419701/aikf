d77a744d69eb42d4f0d2a8f768042ca616756709
qq419701 <qq419701@gmail.com>
2026-03-27 21:34:36 +0800

    Create version.py with initial app configuration

---
+++ version.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+APP_NAME = "AIKF 客服助手"
+APP_VERSION = "3.0.0"
+APP_BUILD = "20260327"
+UPDATE_CHECK_URL = "https://api.github.com/repos/qq419701/aikf/releases/latest"
+GITHUB_REPO = "qq419701/aikf"
